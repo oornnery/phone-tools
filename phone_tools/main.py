d0c9b55275eb708292b87f2ad65d8fa9e019269f
Fabio Souza <fabiohcsouza@outlook.com.br>
2024-01-05 20:10:07 -0300

    metodos e theding

--- phone_tools/main.py
+++ phone_tools/main.py
@@ -1,9 +1,14 @@
-import re, logging, asyncio
-from typing import Iterable, List
+import re, logging, time
+from typing import Iterable, List, Any
 from dataclasses import dataclass
+from multiprocessing import Process, Event, Manager
 import pexpect
 from rich.logging import RichHandler
+import threading
+import signal
 
+
+# Rich Logging
 FORMAT = "%(message)s"
 logging.basicConfig(
     level="NOTSET", 
@@ -15,6 +20,7 @@ logging.basicConfig(
 log = logging.getLogger("rich")
 
 
+# Dataclasses
 @dataclass
 class Command:
     command: str
@@ -26,6 +32,14 @@ class Command:
         if self.debug:
             logging.debug(f'Command: {self.command} - Code: {self.code} - Msg: {self.msg}')
 
+@dataclass
+class Call:
+    number: str
+    id: int
+    states : List[str]
+
+
+# Events
 class HandlerEvents:
     def handler_call(self, number):
         logging.info(f'Calling to {number}')
@@ -35,99 +49,220 @@ class HandlerEvents:
 
     def handler_bye(self):
         logging.info('Bye')
-    
 
 
 
+# PhoneTools
 class PhoneTools(HandlerEvents):
-    def __init__(self, cmddebug: bool = False):
-        self.child = pexpect.spawn('twinkle -c')
+    """
+    Twinkle CLI wrapper async.
+
+    Twinkle version: 1.10.3 - Feb 19, 2022
+    
+    https://github.com/LubosD/twinkle.git
+    
+    This script was based on the project https://github.com/forkcs/twinkle-py.git 
+    """
+    def __init__(self, cmd: List[str] = ['twinkle', '-c'], debug: bool = False):
+        """
+        :param cmd: a command to execute. defaults to ['twinkle', '-c']
+        :param debug: print Twinkle input to console
+        """
+
         self.DEBUG: bool = debug
-        self.running: bool = False
-        self.is_calling: bool = False
+        self.cmd = cmd
+        self._running: bool = False
         self._prev_output: str = ''
-        self._task_run_ = asyncio.create_task(self.run())
+        self._on_call: bool = False
+        self._on_mute: bool = False
+        self.child = pexpect.spawn('twinkle -c')
+        
+    ##############
+    # Properties #
+    ##############
+    @property
+    def running(self):
+        return self._running
 
-    async def __send_command__(self, command, expect) -> Command:
-        self.child.sendline(command)
-        code = 0
-        msg: str = ''
-        return Command(
-            command=command, 
-            code=code, 
-            msg=msg, 
-            debug=self.DEBUG)
+    @running.setter
+    def running(self, value):
+        self._running = value
 
-    # COMMANDS 
-    ###########
-    async def call(self, number: str):
-        call = await self.__send_command__(f'call {number.strip()}', 'received 100 trying')
-        if call.code == 0:
-            self.is_calling = True
-        return call
-    
-    async def bye(self):
-        bye = await self.__send_command__('bye', 'call ended')
-        if bye.code == 0:
-            self.is_calling = False
-        return bye
-    
-    async def user(self):
-        user = await self.__send_command__('user', '')
-        return user
-    
-    async def send_help(self):
-        self.child.sendline('help')
-        help = await self.child.expect('help', async_=True)
-        return {'code': help, 'msg': self.child.after.decode('utf-8')}
+    @property
+    def prev_output(self):
+        return self._prev_output
 
-    async def quit(self):
-        await self.__send_command__('quit', '')
+    @prev_output.setter
+    def prev_output(self, value):
+        self._prev_output = value
 
-    def close(self):
-        self.child.close()
-        self._task_run_.cancel()
+    @property
+    def on_call(self):
+        return self._on_call
 
-    async def run(self):
-        running = await self.child.expect('Twinkle', async_=True)
-        self.running = True if running == 0 else False
+    @on_call.setter
+    def on_call(self, value):
+        self._on_call = value
+
+    @property
+    def on_mute(self):
+        return self._on_mute
+
+    @on_mute.setter
+    def on_mute(self, value):
+        self._on_mute = value
+
+    ############
+    # Commands #
+    ############
+    def __send_command__(self, command) -> Any:
+        """
+        Send command
+        """
         if self.DEBUG:
-            logging.debug(f'Running: {self.running}')
+            log.debug(f'__send_command__: {command}')
+        self.child.sendline(command)
+
+    def __read_stream__(self) -> Iterable[str]:
         while self.running:
             try:
                 # Read output
-                out = self.child.readline().decode("utf-8").strip()
-                if out != self._prev_output:
-                    # Logging
-                    if self.DEBUG:
-                        logging.debug(f'READING: {out}')
-                    if re.search(r'call', out):
-                        self.handler_call(out.split('')[1])
-                    elif re.search(r'Bye', out):
-                        self.handler_bye()
+                line = self.child.readline().decode("utf-8").strip()
+                # Logging
+                if self.DEBUG:
+                    log.debug(f'READING: {line}')
+                if line == self._prev_output \
+                    or line == '':
+                        continue
+                self.prev_output = line
+                yield line
                 # set previous output
-                self._prev_output = out
             except pexpect.exceptions.EOF:
-                # exited
-                self.running = False
+                self.__stop_reding__()
+                self.quit()
             except pexpect.exceptions.TIMEOUT:
-                # nothing happened for a while
-                pass
-            except KeyboardInterrupt:
-                self.running = False
-                await self.quit()
+                line = 'TIMEOUT'
+                log.error(f'{line}')
+                continue
             except Exception as e:
-                logging.error(e)
-            await asyncio.sleep(0.1)  # Introduce a small delay to avoid tight looping
+                log.error(e)
+                continue
+    
+    def __stop_reding__(self):
+        if self.DEBUG:
+            log.debug(f'__stop_reding__:')
+        self.running = False
+    
+    # COMMANDS 
+    ###########
+    def call(self, number: str):
+        self.__send_command__(f'call {number.strip()}')
+        self.on_call = True
 
-async def main():
+    def answer(self):
+        self.__send_command__('answer')
+        self.on_call = True
+    
+    def bye(self):
+        self.__send_command__('bye')
+        self.on_call = False
+
+    def mute(self):
+        self.__send_command__('mute')
+    
+    def un_mute(self):
+        self.__send_command__('mute')
+    
+    def dtmf(self, digits: str):
+        self.__send_command__(f'dtmf {digits.strip()}')
+    
+    def redial(self):
+        self.__send_command__('redial')
+
+    def register(self):
+        self.__send_command__('register')
+    
+    def deregister(self):
+        self.__send_command__('deregister')
+    
+    def fetch_registered(self):
+        self.__send_command__('fetch_reg')
+    
+    def set_line(self, line: str):
+        self.__send_command__(f'line {line.strip()}')
+    
+    def dnd(self):
+        self.__send_command__('dnd')
+    
+    def hold(self):
+        self.__send_command__('hold')
+    
+    def un_hold(self):
+        self.__send_command__('retrieve')
+
+    def user(self, name: str = None) -> None:
+        if name is None:
+            name = ''
+        self.__send_command__(f'user {name}')
+
+    def presence(self, state: str) -> None:
+        self.__send_command__(f'presence {state}')
+
+    def auto_answer(self) -> None:
+        self.__send_command__('auto_answer')
+
+    def quit(self):
+        self.__send_command__('quit')
+        self.__stop_reding__()
+
+    def run(self) -> None:
+        self.running = True
+        if self.DEBUG:
+            logging.debug(f'Running: {self.running}')
+        # Read stream
+        for line in self.__read_stream__():
+            log.info(line)
+            if 'call' in line:
+                number = line.split('call')[1].strip()
+                log.info(f'Calling to {number}')
+                self.handler_call(number)
+            elif 'bye' in line:
+                self.handler_bye()
+            elif 'registering phone' in line:
+                sip = line.split(' ')
+                log.info(f'Registering phone {sip}')
+            time.sleep(0.1)
+
+def main():
     p = PhoneTools(debug=True)
-    await p.call('010326')
-    await asyncio.sleep(3)
-    input('Close')
-    await p.bye()
-    await asyncio.sleep(3)
-    await p.quit()
-    p.close()
+    try:
+        def signal_handler(sig, frame):
+            print("Pressione Ctrl+C novamente para sair.")
+            signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-asyncio.run(main())
+        signal.signal(signal.SIGINT, signal_handler)
+        # Criar uma thread para executar o método run
+        thread = threading.Thread(target=p.run)
+
+        # Iniciar a thread
+        thread.start()
+
+        for _ in range(5):
+            start = input('Iniciar?: ')
+            if start.lower() == 'n':
+                break
+            p.call('010326')
+            time.sleep(5)
+            input('Fechar')
+            p.bye()
+
+    finally:
+        try:
+            # Aguardar a thread terminar antes de chamar p.quit()
+            thread.join()
+        except KeyboardInterrupt:
+            pass  # Ignorar a exceção quando Ctrl+C é pressionado novamente
+
+        p.quit()
+
+main()
